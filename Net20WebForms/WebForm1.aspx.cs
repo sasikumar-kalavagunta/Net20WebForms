0a4ec28bab90c4969618ae7c7514efdb49ec9dcf
madhusudhanvenkatesan-github <65552529+madhusudhanvenkatesan-github@users.noreply.github.com>
2020-08-18 19:57:20 +0530

    uploaded base solution

---
+++ Net20WebForms/WebForm1.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Net20WebForms
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            
+            lblResult.Text = "Successfully submitted with this inputs : " + TextBox1.Text + "  " + TextBox2.Text;
+        }
+    }
+}
